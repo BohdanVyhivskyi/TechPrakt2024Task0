dc74954a0a44d6141ad7a5b5ff3577bee9eb0d30
BohdanVyhivskyi <vt222_vbv@student.ztu.edu.ua>
2024-02-04 16:23:35 +0200

    Added a console project in C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Введення першого числа
+        Console.WriteLine("Введіть перше число:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        // Введення другого числа
+        Console.WriteLine("Введіть друге число:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Розрахунок суми
+        int sum = num1 + num2;
+
+        // Виведення результату
+        Console.WriteLine("Сума двох чисел: " + sum);
+    }
+}
